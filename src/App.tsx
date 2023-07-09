51d577375478ec2c0f31b8400a64d01cc94ab411
Cao Nam <nam52871@donga.edu.vn>
2023-07-09 12:32:44 +0700

    No comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Home from './pages/Home/Home'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 800)
+  }, [])
   return (
     <div className={`dark:bg-backgroundDark dark:text-white`}>
       <Home />
---
+++ src/pages/Home/components/Spinner.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Spinner() {
+  return (
+    <div className='loading loading04'>
+      <span>C</span>
+      <span>A</span>
+      <span>O</span>
+      <span>N</span>
+      <span>A</span>
+      <span>M</span>
+    </div>
+  )
+}
